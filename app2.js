360baf329f5bdd657bcb14545959c56c25daa929
dr-nside <renzo.ramos.a1302@gmail.com>
2025-03-17 00:59:49 -0500

    Actualizar el app2.js e index.html

--- app2.js
+++ app2.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let Amigos = [];
+let NombreIngresado = "";
+
+function agregarAmigo() {
+    let NombreIngresado = document.getElementById("amigo").value;
+    if (NombreIngresado == "" || NombreIngresado == " ") {
+        alert("Por favor ingrese un nombre válido");
+    } else {
+        Amigos.push(NombreIngresado);
+        LimpiarInput();
+        console.log(Amigos);
+        console.log(NombreIngresado);
+        MostrarListaEnPantalla('ul');
+    }
+}
+
+
+function MostrarListaEnPantalla(elemento, list)  {
+    let listaMostrar = document.getElementById(elemento);
+    return;
+};
+
+
+function LimpiarInput() {
+    document.querySelector("#amigo").value = "";
+}
