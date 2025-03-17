4b216806c92d9d610f253e9297feb78588cfc0e0
dr-nside <renzo.ramos.a1302@gmail.com>
2025-03-17 17:04:51 -0500

    Actualizar el app.js

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let Amigos = [];
 
-// Función para agregar un amigo a la lista
-function agregarAmigo() {
-    let NombreIngresado = document.getElementById("amigo").value;
+let Amigos = [];
 
-    if (NombreIngresado === "" || NombreIngresado === " ") {
+//Función para agregar un amigo a la lista
+
+function agregarAmigoLista () {
+    let InputNombre = document.getElementById("amigo").value;
+    
+    if (InputNombre === "" || NombreIngresado == " ") {
         alert("Por favor ingrese un nombre válido");
     } else {
-        if (Amigos.includes(NombreIngresado)) {
+        if (Amigos.includes(InputNombre)) {
             alert("Este nombre ya ha sido agregado");
-            } else {
-                Amigos.push(NombreIngresado);
-                LimpiarInput();
-                MostrarListaEnPantalla("listaAmigos", Amigos);
-                console.log(Amigos);
-                console.log(NombreIngresado);
-            }
- 
-}
-
-// Función para actualizar la lista de amigos en la pantalla
-function MostrarListaEnPantalla() {
-    let listaMostrar = document.getElementById("listaAmigos");
-    listaMostrar.innerHTML = ""; // Limpiar lista antes de actualizar
-}
-
-    Amigos.forEach((amigo) => {
-        let li = document.createElement("li");
-        li.textContent = amigo;
-        listaMostrar.appendChild(li);
-    });
+        } else {
+            Amigos.push(InputNombre);
+            LimpiarInput();
+            console.log(Amigos);
+            console.log(InputNombre);
+        }
+    } 
+    
 }
 
 // Función para limpiar el campo de entrada
